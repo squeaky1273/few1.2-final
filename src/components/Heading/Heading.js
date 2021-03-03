5a4be190881107b0df3c6803b791aad3c34263bf
squeaky1273 <pjriddell22@gmail.com>
2021-03-03 15:15:02 -0800

    completed challenge 2

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Heading() {
+    return (
+        <div className="Heading">
+            <header>
+                <h1>FEW 1.2 Final Assessment<br />Padyn Riddell</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Heading
