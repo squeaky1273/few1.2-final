f62c2973360c141e2bddafea0c4338bb2374d628
squeaky1273 <pjriddell22@gmail.com>
2021-03-03 17:12:32 -0800

    completed challenge 6

--- src/components/Band/Band.js
+++ src/components/Band/Band.js
@@ -1,15 +1,16 @@
 import React from 'react';
-// import Like from '../Like/Like'
+import Like from '../Like/Like'
 import './Band.css';
 
 function Band(props) {
-    const { band_name, fans, formed, origin } = props
+    const { band_name, fans, formed, origin} = props
     return (
         <div className="Band">
             <h3> { band_name } </h3>
             <p>Formed: { formed } </p>
             <p>{ origin }</p>
             <p>Fans: { fans }</p>
+            <Like />
         </div>
     )
 }
---
+++ src/components/Like/Like.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Like.css';
+
+class Like extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { count: 0  }
+    }
+
+    incrementCount = () => {
+        this.setState({ count: this.state.count + 1 });
+      }
+      
+    decrementCount = () => {
+        this.setState({ count: this.state.count - 1 });
+      }
+
+    render() {
+        let { count } =this.state
+        return (
+            <div>
+                <p>Likes: { count } </p>
+                <button onClick={(e) => {this.decrementCount()}}>-</button>
+                <button onClick={(e) => {this.incrementCount()}}>+</button>
+            </div>
+        );
+}
+
+}
+
+export default Like; 
